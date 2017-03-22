0822496fd417b31b57640fde749df1d02af29408
2n2n <ayolachll@gmail.com>
2017-03-22 12:44:31 +0800

    initial commit python udemy

---
+++ python_refresher/lists_tuples_sets.py
@@ -0,0 +1,9 @@
+my_variable = "hello"
+
+grades = [77, 80, 90, 95, 100]
+tuple_grades = (77, 80, 90, 95, 100) # immutable
+set_grades = { 77, 80, 90, 100, 100 } # unique & unordered
+
+def average(grade):
+	pass
+print( set_grades )
